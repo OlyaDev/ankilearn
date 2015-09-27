602170f4d2e4a5fb7d75581568b13ff9e9cb9990
OlyaDev <olya12@live.ru>
2015-09-28 00:39:17 +0300

    changed color contant

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -23,7 +23,8 @@
 			      <div class="col-xs-3 menu-hat"><a href="/portfolio">Анки</a></div>
 			      <div class="col-xs-3 menu-hat raduis-right"><a href="/contacts">Контакты</a></div>
 		    </div>
-
+<br/>
+<br/>
 		    <div class="middle">
 		    		<div class="col-xs-2 submenu">колонка для подменю</div>
 		    		<div class="col-xs-10 content"><?php include 'application/views/'.$content_view; ?></div>
